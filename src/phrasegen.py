502fb22c7b5d8ea60570fd7cb52c1124665fda34
Manvika Satish <manvikasatish@Manvika-Satishs-MacBook-Pro.local>
2024-06-26 16:05:23 -0700

    initial structure

---
+++ src/phrasegen.py
@@ -0,0 +1,23 @@
+import csv
+import os
+
+def loop_through_docs():
+    directory = "documents"
+    for filename in os.scandir(directory):
+        doc = filename
+        prompt = "hello"
+        testlist = call_openai(doc, prompt)
+        postprocess(testlist)
+
+def get_openai_creds():
+    return "foo"
+
+def call_openai(doc, prompt):
+    get_openai_creds()
+    testlist = []
+    return testlist
+
+def postprocess(list):
+    with open('results.csv', 'a', newline='') as file:
+        writetocsv = csv.writer(file)
+        writetocsv.writerow(list)
