b897815b6e6b7df34a20ef64474b4ccd92f48df4
Manvika Satish <manvikasatish@Manvika-Satishs-MacBook-Pro.local>
2024-06-27 13:31:45 -0700

    changes to create csv file in results

--- src/phrasegen.py
+++ src/phrasegen.py
@@ -2,11 +2,11 @@ import csv
 import os
 
 def iterate_docs():
-    directory = "documents"
+    directory = "./documents"
     for filename in os.scandir(directory):
         doc = filename
 
-        prompt1 = open("prompt1.txt", "r")
+        prompt1 = open("prompts/prompt1.txt", "r")
         content = prompt1.read()
 
         testlist = call_openai(doc, content)
@@ -17,10 +17,13 @@ def get_openai_creds():
 
 def call_openai(doc, prompt):
     get_openai_creds()
-    testlist = []
+    testlist = ["docid", "this is an utterance", "paragraph 2"]
     return testlist
 
 def postprocess(list):
-    with open('results.csv', 'a', newline='') as file:
+    # add timestamp
+    with open('results/results.csv', 'a', newline='') as file:
         writetocsv = csv.writer(file)
         writetocsv.writerow(list)
+
+iterate_docs()
