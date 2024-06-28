de1da14668b0d14b1762f65794e52efd69a065de
aarav429 <aarav.rathi@aisera.com>
2024-06-28 11:15:49 -0700

    Revamped the prompting system

--- src/phrasegen.py
+++ src/phrasegen.py
@@ -1,33 +1,31 @@
 import csv
 import os
 import datetime
-import pandas as pd
+from openaigen import *
 
 def iterate_docs():
     directory = "./documents"
     currenttime = datetime.datetime.now()
 
     program_run = ["program run time", currenttime]
+    result_list = []
+    
     with open('results/results.csv', 'a', newline='') as file:
         writetocsv = csv.writer(file)
         writetocsv.writerow(program_run)
 
     for filename in os.scandir(directory):
         doc = filename
+        filepath = directory + "/" + doc.name
 
-        prompt1 = open("prompts/prompt1.txt", "r")
-        content = prompt1.read()
+        prompt = open("prompts/prompt1.txt", "r").read()
+        print(filepath, prompt)
+        # print(send_prompt_with_document(prompt, filepath))
 
-        testlist = call_openai(doc, content)
-        postprocess(testlist, currenttime)
-
-def get_openai_creds():
-    return "foo"
-
-def call_openai(doc, prompt):
-    get_openai_creds()
-    testlist = ["doc id", "this is an utterance", "paragraph 2"]
-    return testlist
+        # result_list.append(send_prompt_with_document(prompt, filepath))
+        # testlist = call_openai(doc, prompt)
+        # postprocess(testlist, currenttime)
+    print(result_list)
 
 def postprocess(list, currenttime):
     # add timestamp
