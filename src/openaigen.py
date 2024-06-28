2eacefaa398992736255260f2d2bf6511d493784
Manvika Satish <manvikasatish@Manvika-Satishs-MacBook-Pro.local>
2024-06-28 12:12:07 -0700

    checks content length

--- src/openaigen.py
+++ src/openaigen.py
@@ -1,6 +1,7 @@
 from openai import AzureOpenAI
 from bs4 import BeautifulSoup
 from PyPDF2 import PdfReader
+import tiktoken
 
 def extractText_html(html_filepath):
   with open(html_filepath, 'r', encoding = 'utf-8') as file:
@@ -15,6 +16,21 @@ def extractText__pdf(pdf_filepath):
       document_text += i.extract_text()
     return document_text
 
+def count_tokens(text):
+    encoding = tiktoken.get_encoding("cl100k_base")
+    num_tokens = len(encoding.encode(text))
+    return num_tokens
+
+def check_content_length(filetext):
+    expected_count = 8192
+    content = []
+    token_count = count_tokens(filetext)
+    if token_count > expected_count:
+       p1, p2 = filetext.split(' ', 1)
+       content = [p1, p2]
+    content = [filetext]
+    return content
+       
 def send_prompt_with_document(filepath, promptNum):
   if filepath.endswith('.pdf'):
     document_text = extractText__pdf(filepath) 
@@ -24,8 +40,7 @@ def send_prompt_with_document(filepath, promptNum):
   title = filepath[filepath.rfind('/')+1 : filepath.rfind('.')]
   
   prompt = open("prompts/prompt" + str(promptNum) + ".txt", "r").read()
-  
-  #add AzureOpenAi credentials
+
   completion = client.chat.completions.create(
     model="gpt4",
     temperature = 0.6,
@@ -37,4 +52,4 @@ def send_prompt_with_document(filepath, promptNum):
 
   return(completion.choices[0].message)
     
-print(send_prompt_with_document("./documents/Tesla_Autopilot.html",2))
+print(send_prompt_with_document("./documents/anaconda-project-readthedocs-io-en-latest.pdf", 2))
