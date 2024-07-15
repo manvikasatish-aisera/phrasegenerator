3f4ff78dab069f4847c6753d5bb866426cdf65f8
Manvika Satish <manvikasatish@Manvika-Satishs-MacBook-Pro.local>
2024-07-15 10:46:44 -0700

    updated

--- src/fetchdata.py
+++ src/fetchdata.py
@@ -20,34 +20,48 @@ def get_document_key(dict, doc_title):
             break
     return doc_key
 
+# will modify once i get access to the API
 def no_section_document():
     print("Try another api, document doesn't contain sections")
     print("or document does not exist.")
 
 def retrieve_data(promptNum):
     utterances = []
+    # right now its uat
     tenant = input('Tenant: ')
     botid = input('Bot ID: ')
+
+    # up for change
+    num_docs = 25 
 
     document_url = f'http://localhost:8088/tenant-server/v1/tenants/{tenant}/external-documents/check-health?botId={botid}' 
     response = requests.get(document_url)
 
-    # implement better error handling, throw exceptions instead of print statements. 
-    if response.status_code != 200:
-        print("Could not find bot. Please try again.")
+    # implement better error handling, throw exceptions instead of print statements. fix this section
+    # if response.status_code != 200:
+    #     print("cluster, tenant, or bot id does not exist or is incorrect. Please try again.")
 
     resp_text = response.text
     dict = json.loads(resp_text)
+    for dic in dict:
+        print(dic)
+
+    print(len(dict))
+
     doc_key = int(input('Document Key: '))
+
     doc_title = get_doc_title(dict, doc_key)
 
-    section_url = f'http://localhost:8088/tenant-server/v1/tenants/{tenant}/external-documents/retrieve-sections?documentKey={doc_key}&isCommitted=true'
-    resp = requests.get(section_url)
+    sections_url = f'http://localhost:8088/tenant-server/v1/tenants/{tenant}/external-documents/retrieve-sections?documentKey={doc_key}&isCommitted=true'
+    resp = requests.get(sections_url)
 
     if resp.status_code != 200:
         # no sections found in the document, try another api
+        numPhrases = input('Number of Phrases: ')
         no_section_document()
+
     else:
+        numPhrases = input('Number of Phrases (per section): ')
         re = resp.text
         section_dict = json.loads(re)
         for section in section_dict:
--- src/phrasegen.py
+++ src/phrasegen.py
@@ -4,15 +4,27 @@ from datetime import datetime
 from openaigen import *
 from fetchdata import * 
 import openpyxl
+import boto3
 
 def iterate_docs(promptNum):
+    cluster = input('Cluster: ')
+    utterances = retrieve_data(promptNum)
+
+    s3 = boto3.client('s3')
+    # s3.meta.client.upload_file('/tmp/hello.txt', 'mybucket', 'hello.txt')
+
+    # s3.upload_file(
+    #     Filename="C:/Users/admin/Desktop/gfg_logo.png",
+    #     Bucket="mygfgbucket",
+    #     Key="firstgfgbucket.png"
+    # )
+
     currenttime = datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
     csv_file = os.path.join("results",f"csvResults-{currenttime}.csv")
     
     workbook = openpyxl.Workbook()
     sheet = workbook.active
 
-    utterances = retrieve_data(promptNum)
     postprocess(utterances,csv_file)
         
     if sheet['A1'].value == None:
