308ff3b6db5189f0311496152022673ed4e05206
Manvika Satish <manvikasatish@Manvika-Satishs-MacBook-Pro.local>
2024-07-26 11:02:06 -0700

    updated api endpoint

--- src/openaigen.py
+++ src/openaigen.py
@@ -39,7 +39,6 @@ def send_prompt_with_document(section, title):
   api_key = os.getenv('OPENAI_API_KEY')
   # api_key = get_openai_token_from_vault(apikeyPath, "OPENAI_API_KEY")
   # api_version = get_openai_version_from_vault(apikeyPath, "version")
-  api_version = 2  
   azure_endpoint = os.getenv('OPENAI_AZURE_ENDPOINT')
   
   
@@ -47,7 +46,6 @@ def send_prompt_with_document(section, title):
 
   client = AzureOpenAI(
         api_key = api_key,
-        api_version = api_version,
         azure_endpoint = azure_endpoint)
   
   completion = client.chat.completions.create(
