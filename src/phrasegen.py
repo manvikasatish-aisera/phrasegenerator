19ed5b6d0169237b953f24085f0dd7ab3a702efd
Manvika Satish <manvikasatish@Manvika-Satishs-MacBook-Pro.local>
2024-07-29 16:26:38 -0700

    removed sleep time

--- src/phrasegen.py
+++ src/phrasegen.py
@@ -7,7 +7,6 @@ from S3export import *
 import openpyxl
 import requests
 import os
-import time
 
 cluster = os.getenv('CLUSTER').lower()
 tenant = os.getenv('TENANT')
@@ -34,7 +33,6 @@ def iterate_docs(cluster, tenant, bot):
 
     print("iterating through the docs")
 
-    time.sleep(100)
     with open(f'/logs/Info_cluster{cluster}_tenant{tenant}_botid{bot}.csv') as file_obj:
         row_count = 0 
         reader_obj = csv.reader(file_obj) 
