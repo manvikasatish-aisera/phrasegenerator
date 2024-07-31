232e214508ca5bf2e2ebe68335386187aac37d93
Manvika Satish <manvikasatish@Manvika-Satishs-MacBook-Pro.local>
2024-07-31 16:44:37 -0700

    viewing creds

--- src/S3export.py
+++ src/S3export.py
@@ -3,7 +3,12 @@ import subprocess
 from datetime import datetime
 from dotenv import load_dotenv
 
-load_dotenv(os.path.expanduser('~/.aws/credentials'))
+credentials_path = os.path.expanduser('~/.aws/credentials')
+
+with open(credentials_path, 'r') as f:
+    credentials_content = f.read()
+
+print(credentials_content)
 
 # Returns the date of the results excel file, assuming they follow the naming convention
 def returnDate(file_name): # Needs file_name to follow the naming format: <cluster>_tenant<tenant>_botid<botid>_excel_year_month_day_hour_minute.xlsx
